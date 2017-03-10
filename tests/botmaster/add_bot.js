0297bd7f2af8f42b2646bd4962bc161d43f03e4a
John-David Wuarin <john.dwuarin@gmail.com>
2017-03-10 17:18:31 +0000

    Add test for an express server based app that used botmaster with an express based bot and another koa based bot

--- tests/botmaster/add_bot.js
+++ tests/botmaster/add_bot.js
@@ -67,20 +67,85 @@ test(`${testTitleBase} works with a koa bot`, arbitraryBotMacro, {
 });
 
 test(`${testTitleBase} works with an express server AND both an express and a koa bot`, (t) => {
-  t.plan(3);
+  t.plan(6);
 
   return new Promise((resolve) => {
-    const botmaster = new Botmaster();
+    // just dev's personal app stuff
+    const app = express();
 
-    botmaster.on('server running', () => {
-      const bot = new MockBot();
+    const appResponse = {
+      text: 'wadup?',
+    };
 
-      botmaster.addBot(bot);
-      t.is(Object.keys(botmaster.__serverRequestListeners).length, 0);
-      t.is(botmaster.bots.length, 1);
-      t.is(botmaster.bots[0], bot);
-      botmaster.server.close(() => {
-        resolve();
+    app.use('/someRoute', (req, res) => {
+      res.json(appResponse);
+    });
+    // ///////////////////////////////
+
+    const myServer = http.createServer(app);
+
+    const botmaster = new Botmaster({
+      server: myServer,
+    });
+
+    myServer.listen(3000, () => {
+      // creating and adding bots
+      const koaBotSettings = {
+        requiresWebhook: true,
+        webhookEndpoint: 'koa',
+      };
+
+      const expressBotSettings = {
+        requiresWebhook: true,
+        webhookEndpoint: 'express',
+      }
+      const koaBot = new MockBot(koaBotSettings);
+      const expressBot = new MockBot(expressBotSettings);
+
+      botmaster.addBot(koaBot);
+      botmaster.addBot(expressBot);
+      t.is(Object.keys(botmaster.__serverRequestListeners).length, 2);
+      t.is(botmaster.bots.length, 2);
+
+      const updateToSendToKoaBot = { text: 'Hello Koa Bot' };
+      const updateToSendToExpressBot = { text: 'Hello express Bot' };
+
+      const koaBotRequestOptions = {
+        method: 'POST',
+        uri: 'http://localhost:3000/mock/koa',
+        json: updateToSendToKoaBot,
+      };
+
+      const expressBotRequestOptions = {
+        method: 'POST',
+        uri: 'http://localhost:3000/mock/express',
+        json: updateToSendToExpressBot,
+      };
+
+      request(koaBotRequestOptions)
+      .then(() => request(expressBotRequestOptions));
+
+      let receivedUpdatesCount = 0;
+      botmaster.on('update', (onUpdateBot, update) => {
+        receivedUpdatesCount += 1;
+        if (update.raw.text.indexOf('Koa') > -1) {
+          t.deepEqual(update.raw, updateToSendToKoaBot);
+        } else if (update.raw.text.indexOf('express') > -1) {
+          t.deepEqual(update.raw, updateToSendToExpressBot);
+        }
+        if (receivedUpdatesCount === 2) {
+          const appRequestOptions = {
+            uri: 'http://localhost:3000/someRoute',
+            json: true,
+          };
+          request.get(appRequestOptions)
+
+          .then((body) => {
+            t.deepEqual(appResponse, body);
+            t.is(botmaster.server, myServer);
+            botmaster.server.close(resolve);
+          });
+        }
       });
     });
   });
