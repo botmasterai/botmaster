9c72b22a49955007e8ce9f631c7a27d68099561f
John-David Wuarin <john.dwuarin@gmail.com>
2017-03-17 19:38:49 +0000

    Remove reference to tests index in main index. Users should use require('botmaster/tests') to have access to MockBot

--- tests/index.js
+++ tests/index.js
@@ -2,6 +2,16 @@
 
 const MockBot = require('./_mock_bot');
 
+// if using MockBot in your library, just do a require('botmaster/tests').MockBot
+// and make sure the following packages are either in your dependencies exports
+// dev-dependencies:
+
+/**
+ * express
+ * koa
+ * body-parser
+ */
+
 module.exports = {
   MockBot,
 };
