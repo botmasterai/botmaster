835a0b20dbf919449a534cce0f62568517d26cef
John-David Wuarin <john.dwuarin@gmail.com>
2017-04-01 15:08:27 +0100

    Add more verbose debug output for adding wrapped middleware

--- tests/.eslintrc.js
+++ tests/.eslintrc.js
@@ -1,13 +1,14 @@
 module.exports = {
   extends: 'airbnb',
   plugins: [
-    'ava'
+    'ava',
   ],
   rules: {
-    'import/no-extraneous-dependencies': ['off'],
-    'no-underscore-dangle': ["off"],
+    'import/no-extraneous-dependencies': 'off',
+    'no-underscore-dangle': 'off',
     semi: [2, 'always'],
     'no-param-reassign': 'off',
     'no-restricted-syntax': 'off',
-  }
+    'strict': 'off',
+  },
 };
